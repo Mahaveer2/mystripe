649fe5e5b155e12851590bf0b9a72ebdce413547
Partab Rai <partabrai@Partabs-MacBook-Pro.local>
2022-06-20 13:46:20 +0500

    little bug fixes

--- components/Nav.js
+++ components/Nav.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
 
 function Nav() {
   const [navbar,setNavbar] = useState(false);
@@ -21,10 +22,11 @@ function Nav() {
   return (
       <nav className={!navbar ? ('bg-gradient-to-b from-purple-400 to-indigo-500 px-2 sm:px-4 py-2.5 text-white dark:bg-gray-800'+ (active && ' h-[40vh]')) :'px-2 py-2.5 bg-white z-10 fixed w-full border-b border-gray-200 top-0' + (active && ' h-[40vh]')}> 
   <div className="container flex flex-wrap justify-between items-center mx-auto">
-    <a href="/" className="flex items-center">
+    <Link href="/" >
+    <a className="flex items-center">
         <img src="https://uploads.tikroyal.com/cdn/gallery/r6pb26sifc6pdd4m0bab.png" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
 
-    </a>
+    </a></Link>
     <button onClick={() => setActive(!active)} data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
       <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -33,7 +35,9 @@ function Nav() {
     <div className={active ? ' w-full md:block md:w-auto':'md:block hidden'} id="mobile-menu">
       <ul className={!navbar ? 'white-t flex  flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium':'flex  flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium'}>
         <li>
-          <a href="/" className={!navbar ? 'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-white-700 md:p-0 ' : 'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'} aria-current="page">Home</a>
+          <Link href="/" >
+          <a className={!navbar ? 'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-white-700 md:p-0 ' : 'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'} aria-current="page">Home</a>
+          </Link>
         </li>
          <li>
           <a href="#" onClick={() => window.scroll({
@@ -51,7 +55,9 @@ function Nav() {
           })} className={!navbar ? 'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-white-700 md:p-0 ' : 'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'}>Pricing</a>
         </li>
         <li>
-          <a href="contact/" className={!navbar ? 'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-white-700 md:p-0 ' : 'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'}>Contact</a>
+          <Link href="contact/" >
+          <a className={!navbar ? 'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-white-700 md:p-0 ' : 'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'}>Contact</a>
+          </Link>
         </li>
       </ul>
     </div>
